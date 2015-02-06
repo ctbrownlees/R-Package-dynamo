ff204e98c8466f415ca46376c5ae2a182674845a
Christian <ctbrownlees@gmail.com>
2015-02-06 02:39:52 +0100

    prg?

--- R/dynamo.R
+++ R/dynamo.R
@@ -16,23 +16,25 @@
     job <- list( mdl=mdl , y=y )
 }
 
-
 `dm` <-
-function(formula,data=parent.frame(),param=NULL,opts=NULL){
+function(formula,data=parent.frame(),opts=NULL){
 
-	call <- match.call()
+  call <- match.call()
   
   job <- dm.spec(formula,data)
   
   est <- switch( job$mdl ,
-          garch=garch.fit(job$y),
-          tarch=tarch.fit(job$y)
+          garch=garch.fit(job$y,opts),
+          tarch=tarch.fit(job$y,opts),
+          mewma=mewma.fit(job$y,opts)
   )
   
-	obj <- list( call=call )
-	class(obj) <- 'dm'
+  obj <- list( call=call )
+  class(obj) <- 'dm'
+
   obj$model  <- job$mdl
   obj$coef   <- est$param
+  obj$vcv    <- est$vcv
 
   obj
 }
@@ -67,29 +69,35 @@ garch.filter <- function( y , param ){
 		as.integer(T) , 
 		PACKAGE="dynamo" )
 
-  filter = list( sigma2=result$sigma2 , loglik=result$loglik )
+        filter = list( sigma2=result$sigma2 , loglik=result$loglik )
   
 	return(filter)
 }
 
-garch.fit <- function(y){
+garch.fit <- function(y,opts){
   
   obj  <- function(x){ return( -garch.filter(y,x)$loglik ) }  
-  der  <- function(x){ return( grad(obj,x) ) }
+  der  <- function(x){ return( nl.grad(x,obj) ) }
   
   # initial values
   x0 <- c( var(y)*(0.05) , 0.05 , 0.90 )
   
-  opts <- list("algorithm"="NLOPT_LD_LBFGS",
-               "xtol_rel"=1.0e-8)
+  if( is.null(opts$fit) | opts$fit==FALSE )
+  {
+	  opts <- list("algorithm"="NLOPT_LD_LBFGS",
+        	       "xtol_rel"=1.0e-8)
   
-  #
-  res <- nloptr( x0=x0, 
+	  res <- nloptr( x0=x0, 
                  eval_f     =obj,
                  eval_grad_f=der, 
                  opts=opts)
- 
-  list( param=res$solution )
+  }
+  
+  print( res )
+
+  #C <- solve( hessian( obj , res$solution ) )
+  
+  list( param=res$solution , param.names <-c('intercept','ARCH','GARCH') , vcv=C)
 }
 
 tarch.filter <- function( y , param ){
@@ -114,7 +122,7 @@ tarch.filter <- function( y , param ){
 tarch.fit <- function(y){
   
   obj  <- function(x){ return( -tarch.filter(y,x)$loglik ) }  
-  der  <- function(x){ return( grad(obj,x) ) }
+  der  <- function(x){ return( nl.grad(x,obj) ) }
   
   # initial values
   x0 <- c( var(y)*(0.05) , 0.05 , 0.0 , 0.90 )
@@ -132,3 +140,52 @@ tarch.fit <- function(y){
   
   list( param=res$solution )
 }
+
+mewma.filter <- function( y , param ){
+
+	T      <- nrow(y)
+	N      <- ncol(y)
+
+	result <- .C( 'mewma_filter', 
+		status = as.integer(0), 
+		Sigma  = as.double(rep(0,T*N)) , 
+		eps    = as.double(rep(0,T*N)) , 
+		loglik = as.double(0) , 
+		as.double(param) , 
+		as.double(y) , 
+		as.integer(T) , 
+		as.integer(N) , 
+		PACKAGE="dynamo" )
+
+  	filter = list( sigma2=result$sigma2 , loglik=result$loglik )
+  
+	return(filter)
+}
+
+mewma.fit <- function(y,param,fit){
+  
+  obj  <- function(x){ return( -mewma.filter(y,x)$loglik ) }  
+  der  <- function(x){ return( nl.grad(x,obj) ) }
+  
+  # initial values
+  if( fit==TRUE ){
+	  x0 <- c( var(y)*(0.05) , 0.05 , 0.90 )
+  
+	  opts <- list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8)
+  
+	  res <- nloptr( x0=x0, 
+                 eval_f     =obj,
+                 eval_grad_f=der, 
+                 opts=opts)
+  
+	  print( res )
+  }
+  else {
+	out <- mewma.filter(y,param)
+  }  
+
+  #C <- solve( hessian( obj , res$solution ) )
+  
+  list( param=res$solution , param.names <-c('intercept','ARCH','GARCH') , vcv=C)
+}
+
--- src/dynamo.c
+++ src/dynamo.c
@@ -3,22 +3,23 @@
 #include <math.h>
 
 // GARCH(1,1) Model Filter
-void garch_filter(int *status, double *sigma2, double* eps, double *loglik, double *param, double *y, int *T){
+void garch_filter(int *status, double *sigma2, double* eps, double *loglik, double *dloglik, double *param, double *y, int *T){
 
 	double logden;
+	double dlogden[3];
 	double omega, alpha, beta;
-  int t;
+	int t;
 
 	omega = param[0];
 	alpha = param[1];
 	beta  = param[2];
   
-  if( alpha <= 0 || beta < 0 || omega<0 || (alpha+beta)>1 ){
-    *loglik = -10000000;
-    return;
-  }
+	if( alpha <= 0 || beta < 0 || omega<0 || (alpha+beta)>1 ){
+		*loglik = -10000000;
+		return;
+	}
 
-  *loglik = 0;
+	*loglik = 0;
 
 	// init sigma2
 	sigma2[0]=0;
@@ -27,16 +28,16 @@ void garch_filter(int *status, double *sigma2, double* eps, double *loglik, doub
 	}
 	sigma2[0] /= 10;
   
-  //
+	//
 	eps[0] = y[0]/sqrt( sigma2[0] );
   
 	// garch loop 
 	for( t=1 ; t<*T ; ++t ){
 		sigma2[t] = omega + alpha * y[t-1]*y[t-1] + beta * sigma2[t-1];	
 		eps[t]    = y[t]/sqrt( sigma2[t] );
+
 		logden    = log( sigma2[t] ) -0.5 * (eps[t]*eps[t])/sigma2[t];
-    
-    *loglik   += logden;
+		*loglik   += logden;
 	}
 
 }
@@ -45,13 +46,13 @@ void garch_filter(int *status, double *sigma2, double* eps, double *loglik, doub
 void tarch_filter(int *status, double *sigma2, double* eps, double *loglik, double *param, double *y, int *T){
 
   double logden;
-	double omega, alpha, gamma, beta;
+  double omega, alpha, gamma, beta;
   int t;
 
-	omega = param[0];
-	alpha = param[1];
+  omega = param[0];
+  alpha = param[1];
   gamma = param[2];
-	beta  = param[3];
+  beta  = param[3];
   
   if( alpha <= 0 || beta < 0 || omega<0 || (alpha+0.5*gamma+beta)>1 ){
     *loglik = -10000000;
@@ -60,23 +61,62 @@ void tarch_filter(int *status, double *sigma2, double* eps, double *loglik, doub
 
   *loglik = 0;
 
-	// init sigma2
-	sigma2[0]=0;
-	for( t=0; t<10; ++t ){
-		sigma2[0] += y[t]*y[t];
-	}
-	sigma2[0] /= 10;
-  
+  // init sigma2
+  sigma2[0]=0;
+  for( t=0; t<10; ++t ){
+    sigma2[0] += y[t]*y[t];
+  }
+  sigma2[0] /= 10;
+
   //
-	eps[0] = y[0]/sqrt( sigma2[0] );
-  
-	// garch loop 
-	for( t=1 ; t<*T ; ++t ){
-		sigma2[t] = omega + alpha * y[t-1]*y[t-1] + gamma * y[t-1]*y[t-1]*((double)(y[t-1]<0)) + beta * sigma2[t-1];	
-		eps[t]    = y[t]/sqrt( sigma2[t] );
-		logden    = log( sigma2[t] ) -0.5 * (eps[t]*eps[t])/sigma2[t];
-    
+  eps[0] = y[0]/sqrt( sigma2[0] );
+
+  // garch loop 
+  for( t=1 ; t<*T ; ++t ){
+    sigma2[t] = omega + alpha * y[t-1]*y[t-1] + gamma * y[t-1]*y[t-1]*((double)(y[t-1]<0)) + beta * sigma2[t-1];	
+    eps[t]    = y[t]/sqrt( sigma2[t] );
+    logden    = log( sigma2[t] ) -0.5 * (eps[t]*eps[t])/sigma2[t];
+
     *loglik   += logden;
-	}
+  }
 
 }
+
+// MEWMA Model Filter
+void mewma_filter(int *status, double *Sigma, double* eps, double *loglik, double *param, double *y, int *T, int *N){
+
+	double logden;
+	double lambda;
+	int t,i,j;
+	double ***S, **E, **Y;
+ 
+  	*loglik = 0;
+
+	S = Calloc(T,double**);
+	for( t=0; t<T; t++) { 
+		S[t] = Calloc(N,double *); 
+		for( i=0; i<N; ++i ) {
+			S[t][i] = Calloc(N,double); 
+			for( i=0; i<N; ++i ){
+				S[t][i][i] = 0;
+			}
+		}
+	}
+
+	Y = Calloc(T,double*);
+	E = Calloc(T,double*);
+	for( i=0; i<T; i++) { 
+		Y[i] = Calloc(N,double); 
+		E[i] = Calloc(N,double); 
+		for( j=0; j<N; j++ ) {
+			Y[i][j] = y[ M * j + i ];
+			E[i][j] = 0;
+		}
+	}
+
+	// clean up	
+	for(t = 0; t < T; t++){ Free(Y[i]); Free(E[i])}
+	Free(Y);
+	Free(E);
+}
+
