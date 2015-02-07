4c254f0a7f60fe93723fc93bdedf18c6e195bba9
Christian <ctbrownlees@gmail.com>
2015-02-07 17:46:48 +0100

    prg

--- R/dynamo.R
+++ R/dynamo.R
@@ -6,7 +6,64 @@
      library.dynam("dynamo", pkg, lib)
 }
 
-`dm.spec` <- function( frml, data){
+# dynamo interface 
+`dm` <- function(formula,data=parent.frame(),opts=NULL){
+
+	call <- match.call()
+
+	job <- dm.spec(formula,data)
+
+	est <- switch( job$mdl ,
+	  garch=garch.fit(job$y,opts),
+	  tarch=tarch.fit(job$y,opts),
+	  mewma=mewma.fit(job$y,opts)
+	)
+
+	obj <- list( call=call )
+	class(obj) <- 'dm'
+
+	# model info
+	obj$model  <- job$mdl
+
+	# coef
+	obj$coef          <- est$param
+	obj$vcv           <- est$vcv
+	se.coef           <- sqrt(diag(obj$vcv))
+	tval              <- obj$coef/se.coef
+	matcoef           <- cbind( obj$coef , se.coef, tval, 2*(1-pnorm(abs(tval))))
+	dimnames(matcoef) <- list( est$param.names , c(" Estimate"," Std. Error", " t value", "Pr(>|t|)"))
+	obj$matcoef       <- matcoef
+
+	return(obj)
+}
+
+`predict.dm` <- function(x) {
+
+}
+
+`print.dm` <- function (x, digits = max(3, getOption("digits") - 3), ...) {
+	cat("\nCall:\n", deparse(x$call) , sep = "")
+	cat("\n\nCoefficient(s):\n")
+	printCoefmat(x$matcoef, digits = digits, signif.stars = TRUE)
+	invisible(x)
+}
+
+`plot.dm` <- function( x , ... ){
+}
+
+`summary.dm` <- function( x , ... ){
+	invisible(x)
+}
+
+print.summary.dm <- function( x , ... ){
+	cat("\nCall:\n", deparse(x$call), "\n\n", sep = "")
+
+	#cat("\nCoefficient(s):\n")
+	#printCoefmat(x$matcoef, digits = 6, signif.stars = TRUE)
+}
+
+# Utilities
+dm.spec <- function( frml, data){
 
     # variable initialization
     frml.char <- as.character(frml) 
@@ -16,50 +73,28 @@
     job <- list( mdl=mdl , y=y )
 }
 
-`dm` <-
-function(formula,data=parent.frame(),opts=NULL){
-
-  call <- match.call()
-  
-  job <- dm.spec(formula,data)
-  
-  est <- switch( job$mdl ,
-          garch=garch.fit(job$y,opts),
-          tarch=tarch.fit(job$y,opts),
-          mewma=mewma.fit(job$y,opts)
-  )
-  
-  obj <- list( call=call )
-  class(obj) <- 'dm'
-
-  obj$model  <- job$mdl
-  obj$coef   <- est$param
-  obj$vcv    <- est$vcv
-
-  obj
+vcv.mle <- function( x , obj , epsilon ){
+	npar    <- length(x)
+	Hessian <- matrix(0,npar,npar)
+	for (i in 1:npar) {
+		for (j in 1:npar) {
+			x1 = x2 = x3 = x4 = x
+			x1[i] = x1[i] + epsilon[i]; x1[j] = x1[j] + epsilon[j]
+			x2[i] = x2[i] + epsilon[i]; x2[j] = x2[j] - epsilon[j]
+			x3[i] = x3[i] - epsilon[i]; x3[j] = x3[j] + epsilon[j]
+			x4[i] = x4[i] - epsilon[i]; x4[j] = x4[j] - epsilon[j]
+			Hessian[i, j] = (obj(x1)-obj(x2)-obj(x3)+obj(x4))/ (4*epsilon[i]*epsilon[j])
+		}
+	}
+	vcv <- solve(Hessian)
 }
 
-print.dm <- function (x, digits = max(3, getOption("digits") - 3), ...) {
-    cat("\nCall:\n", deparse(x$call), "\n\n", sep = "")
-    cat("\n")
-    invisible(x)
-}
-
-plot.dm <- function( x , ... ){
-}
-
-summary.dm <- function( x , ... ){
-}
-
-print.summary.dm <- function( x , ... ){
-	cat("\nCall:\n", deparse(x$call), "\n\n", sep = "")
-}
-
+# Gaussian GARCH(1,1) functions
 garch.filter <- function( y , param ){
 
 	T      <- length(y)
 
-	result <- .C( 'garch_filter', 
+	filter <- .C('garch_filter', 
 		status = as.integer(0), 
 		sigma2 = as.double(rep(0,T)) , 
 		eps    = as.double(rep(0,T)) , 
@@ -69,7 +104,7 @@ garch.filter <- function( y , param ){
 		as.integer(T) , 
 		PACKAGE="dynamo" )
 
-        filter = list( loglik=result$loglik , sigma2=result$sigma2 , eps=results$eps )
+        filter = list( loglik=filter$loglik , sigma2=filter$sigma2 , eps=filter$eps )
   
 	return(filter)
 }
@@ -91,28 +126,31 @@ garch.fit <- function(y,opts){
 	}
 
 	# MAIN
+	obj  <- function(x){ return( -garch.filter(y,x)$loglik ) }  
+
 	if( fit==TRUE ){ 
-
-		obj  <- function(x){ return( -garch.filter(y,x)$loglik ) }  
-
 		res <- nlminb( param.init, obj, lower=c(0,0,0), upper=c(1,1,1) )
 		param.est <- res$par
-
-		print( param.est )
 	}
 	else {
 		param.est <- param.init 
 	}
 
 	filter <- garch.filter(y,param.est)
+	vcv    <- vcv.mle( param.est , obj , 0.0001 * param.est )
 
-	#C <- solve( hessian( obj , res$solution ) )
-
-	list( param=param.est , param.names=c('intercept','ARCH','GARCH') , 
-		fitted=filter$sigma2 , fitted.names='sigma2', 
-		residuals=1 )
+	list(   param=param.est   , param.names=c('intercept','ARCH','GARCH') , 
+		fit=filter$sigma2 , fit.names='sigma2', 
+		resid=filter$eps  , res.names='res',
+		vcv=vcv ,
+                loglik=filter$loglik )
 }
 
+garch.predict <- function( x ){
+
+}
+
+# Gaussian TARCH(1,1) functions
 tarch.filter <- function( y , param ){
   
   T      <- length(y)
@@ -154,6 +192,7 @@ tarch.fit <- function(y){
   list( param=res$solution )
 }
 
+# Multivariate EWMA
 mewma.filter <- function( y , param ){
 
 	T      <- nrow(y)
